287a229faaca3667cd7ed532f85cdb9a9748265d
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2025-03-03 15:29:25 +0530

    add factory for creating models

---
+++ src/models/model_factory.py
@@ -0,0 +1,37 @@
+from models.base_model import BaseModel
+from models.hps_v1_model import HPSv1Model
+from models.hps_v2_model import HPSv2Model
+from models.stable_diffusion_model import StableDiffusionModel
+
+class ModelFactory:
+    @staticmethod
+    def create_model(
+        model_type: str, model_path: str,
+        offload_to_cpu: bool = False,
+        resolution: int = None,
+        **kwargs,
+    ) -> BaseModel:
+        """
+        Creates and returns an instance of a model subclass based on the model_type.
+
+        Args:
+            model_type (str): The type of model to create. Supported values are:
+                - "hpsv1": For HPSv1 reward models.
+                - "hpsv2": For HPSv2 reward models.
+                - "sd": For stable diffusion text-to-image models.
+            model_path (str): The path or repository ID of the model checkpoint.
+
+        Returns:
+            BaseModel: An instance of the requested model.
+
+        Raises:
+            ValueError: If an unsupported model_type is provided.
+        """
+        if model_type == "hpsv1":
+            return HPSv1Model(model_path)
+        elif model_type == "hpsv2":
+            return HPSv2Model(model_path)
+        elif model_type == "sd":
+            return StableDiffusionModel(model_path, offload_to_cpu, resolution, **kwargs)
+        else:
+            raise ValueError("Unsupported model type. Use 'sd' for stable diffusion models or 'hps' for HPS models.")
