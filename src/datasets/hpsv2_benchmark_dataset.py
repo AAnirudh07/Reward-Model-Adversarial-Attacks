c05359eb17f5498b8eba52bcd5c50ca6eaeb26e0
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2025-03-04 08:58:27 +0530

    create hpsv2 dataset

---
+++ src/datasets/hpsv2_benchmark_dataset.py
@@ -0,0 +1,9 @@
+import hpsv2
+from typing import Dict, List
+
+from datasets.base_prompt_dataset import BasePromptDataset
+
+class HPSV2PromptDataset(BasePromptDataset):
+    def load_dataset(self) -> Dict[str, List[str]]:
+        all_prompts = hpsv2.benchmark_prompts("all")
+        return all_prompts.items()
