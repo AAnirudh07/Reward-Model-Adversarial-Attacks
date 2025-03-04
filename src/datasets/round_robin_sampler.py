dce0a3dac92d19ec54cfbd472243112df258aaa8
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2025-03-04 09:02:55 +0530

    remove redundant code

--- src/datasets/round_robin_sampler.py
+++ src/datasets/round_robin_sampler.py
@@ -6,7 +6,6 @@ class RoundRobinSampler(torch.utils.data.Sampler):
     def __init__(self, dataset: BasePromptDataset):
         self.dataset = dataset
         self.indices = self._generate_indices()
-        self.current_index = 0  # Pointer to maintain state across iterations
 
     def _generate_indices(self):
         """
@@ -17,10 +16,10 @@ class RoundRobinSampler(torch.utils.data.Sampler):
         to the flat sample index.
         
         In our round-robin samples, the ordering is:
-            index 0: category1, index 1: category2, ..., index N: category1
-            
-        Let K = number of categories.
-        Then the sample index for category j at position i is: i * K + j.
+        index 0: category1, index 1: category2, ..., index N: category1
+        
+        Let K = number of categories,
+        Then the sample index for category j at position i is: i*K + j.
         """
         categories = list(self.dataset.data.keys())
         num_per_category = len(next(iter(self.dataset.data.values())))
@@ -39,19 +38,7 @@ class RoundRobinSampler(torch.utils.data.Sampler):
         return ordered_indices
 
     def __iter__(self):
-        """
-        Yield the full list of indices starting from the current_index.
-        After yielding all indices, update current_index so that the next call starts at a different offset.
-        """
-        N = len(self.indices)
-        start = self.current_index
-
-        # Yield indices from current_index to end, then wrap-around.
-        for i in range(N):
-            yield self.indices[(start + i) % N]
-        
-        # Update current_index for next iteration. Here we shift by one.
-        self.current_index = (self.current_index + 1) % N
+        return iter(self.indices)
 
     def __len__(self):
         return len(self.indices)
