96639a706977c360421201d58b49ef01a7972867
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2025-03-05 09:39:04 +0530

    create dataset factory

---
+++ src/datasets/dataset_factory.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+from datasets.base_prompt_dataset import BasePromptDataset
+from datasets.drawbench_dataset import DrawBenchPromptDataset
+from datasets.hpsv2_benchmark_dataset import HPSV2PromptDataset
+from datasets.image_prompt_dataset import ImagePromptDataset
+
+class DatasetFactory:
+    @staticmethod
+    def create_dataset(
+        dataset_type: str,
+        **kwargs,
+    ) -> Union[BasePromptDataset, ImagePromptDataset]:
+        
+        if dataset_type == "drawbench":
+            return DrawBenchPromptDataset()
+        elif dataset_type == "hpsv2":
+            return HPSV2PromptDataset()
+        elif dataset_type == "imageandprompt":
+            return ImagePromptDataset(**kwargs)
+        else:
+            raise ValueError(f"Unknown dataset type: '{dataset_type}'.")
