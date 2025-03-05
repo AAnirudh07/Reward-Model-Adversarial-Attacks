2fe73cca33ed1d853f16ec38a6c099b3c74405a4
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2025-03-05 06:42:48 +0530

    add fgsm attack for reward models

---
+++ src/attacks/white_box/fgsm_reward_model.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+
+from torchattacks.attack import Attack
+
+class FGSMRewardModel(Attack):
+    """
+    FGSM for reward models.
+    
+    Instead of using the cross-entropy loss, this attack uses a custom loss:
+    Loss = -reward, so that the adversary minimizes the reward score.
+    
+    Distance Measure : Linf
+    
+    Arguments:
+        model (BaseModel): reward model to attack.
+        eps (float): maximum perturbation. (Default: 8/255)
+    """
+    def __init__(self, model, eps=8/255):
+        super().__init__("FGSMRewardModel", model)
+        self.eps = eps
+        self.supported_mode = ["default"]
+
+    def forward(self, images):
+        """
+        Overridden forward method for attacking a reward model.
+        """
+
+        images = images.clone().detach().to(self.device)
+        images.requires_grad = True
+
+        reward = self.model.inference(images)
+
+        # Define loss as negative reward, so minimizing loss reduces the reward.
+        cost = -reward
+
+        grad = torch.autograd.grad(
+            cost, images, retain_graph=False, create_graph=False
+        )[0]
+
+        adv_images = images + self.eps * grad.sign()
+        adv_images = torch.clamp(adv_images, min=0, max=1).detach()
+
+        return adv_images
