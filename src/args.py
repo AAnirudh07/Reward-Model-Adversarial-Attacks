d5dda27cff4437894d94cc742b0cb299ff156efe
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2025-03-06 08:51:20 +0530

    update args

--- src/args.py
+++ src/args.py
@@ -9,7 +9,7 @@ def check_target_model(value):
     return value
 
 def check_reward_model(value):
-    valid_versions = ["v1.0", "v2.0", "v2.1"]
+    valid_versions = ["v1.0", "v2.0"]
     if value not in valid_versions:
         raise argparse.ArgumentTypeError(
             "reward_model_name must be one of: 'v1.0', 'v2.0'.")
@@ -27,11 +27,11 @@ def check_attack_name(value):
             "attack_name must be one of: 'gn', 'fgsm', 'pgd', 'spsa'.")
     return value
 
-def parse_args():
+def parse_model_args():
     parser = argparse.ArgumentParser(
-        description="Argument parser for image generation and attack process."
+        description="Argument partser for image generation process."
     )
-    
+
     # Models group
     models = parser.add_argument_group("models")
     models.add_argument("--target_model_name", type=check_target_model, required=True,
@@ -50,11 +50,6 @@ def parse_args():
     datasets.add_argument("--shuffle", action="store_true",
                         help="Shuffle prompts prior to sampling (default: False)")
 
-    # Attack group
-    attack = parser.add_argument_group("attack")
-    attack.add_argument("--attack_name", type=check_attack_name, required=True,
-                        help="Name of the perturbation attack: gn, fgsm, pgd, or spsa")
-
     # Misc group
     misc = parser.add_argument_group("misc")
     misc.add_argument("--inference_batch_size", type=int, default=4,
@@ -62,15 +57,55 @@ def parse_args():
     misc.add_argument("--no_save_image_results", dest="save_image_results", action="store_false",
                         help="Do not store images, prompts, and reward scores that pass threshold")
     misc.set_defaults(save_image_results=True)
-    misc.add_argument("--use_cached_results", action="store_true", default=False,
-                        help="Use stored images, prompts, and reward scores (default: False)")
 
     args = parser.parse_args()
-
     if args.num_samples_per_category is None:
         if args.dataset_name == "hps":
             args.num_samples_per_category = 5
         else:  # drawbench
             args.num_samples_per_category = 2
+    return args
 
+def parse_attack_args():
+    parser = argparse.ArgumentParser(
+        description="Argument partser for attack process."
+    )
+
+    # Models group
+    models = parser.add_argument_group("models")
+    models.add_argument("--reward_model_name", type=check_reward_model, required=True,
+                        help="HPS reward model version: v1.0, v2.0")
+
+    # Attack group
+    attack = parser.add_argument_group("attack")
+    attack.add_argument("--attack_name", type=check_attack_name, required=True,
+                        help="Name of the perturbation attack: gn, fgsm, pgd, or spsa")
+    
+    # Misc group
+    misc = parser.add_argument_group("misc")
+    misc.add_argument("--saved_images_path", type=str, required=True,
+                        help="Path where base images and prompts are stored")
+    misc.add_argument("--attack_batch_size", type=int, default=8,
+                        help="Batch size for PGD & FGSM attack (default: 8)")
+
+    args = parser.parse_args()
+    return args
+
+
+def parse_transfer_attack_args():
+    parser = argparse.ArgumentParser(
+        description="Argument partser for attack process."
+    )
+
+    # Models group
+    models = parser.add_argument_group("models")
+    models.add_argument("--reward_model_name", type=check_reward_model, required=True,
+                        help="HPS reward model version: v1.0, v2.0")
+
+    # Misc group
+    misc = parser.add_argument_group("misc")
+    misc.add_argument("--saved_images_path", type=str, required=True,
+                        help="Path where adversarial images are stored")
+
+    args = parser.parse_args()
     return args
